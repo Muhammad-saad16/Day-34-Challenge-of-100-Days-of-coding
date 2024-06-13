9043ca7027f2d1340a68dac5438835b98e38419c
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2024-06-13 14:52:18 +0500

    Day 34

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+//                      🚀 Day 34 Challenge: Start Coding! 🚀
+Object.defineProperty(exports, "__esModule", { value: true });
+// Question 100:
+// Use the JavaScript Math object to find the square root of 144.
+let squareRoot = Math.sqrt(4);
+console.log(squareRoot);
+// Question 101:
+// Generate a random integer between 1 and 10.
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log(randomNumber);
+// Question 102:
+// Calculate and log the absolute difference between the number -5 and 5.
+let absoluteDifference = Math.abs(-5 - 5);
+console.log(absoluteDifference);
---
+++ main.ts
@@ -0,0 +1,23 @@
+//                      🚀 Day 34 Challenge: Start Coding! 🚀
+
+
+// Question 100:
+// Use the JavaScript Math object to find the square root of 144.
+
+let squareRoot: number = Math.sqrt(4)
+console.log(squareRoot);
+
+
+// Question 101:
+// Generate a random integer between 1 and 10.
+
+
+let randomNumber: number = Math.floor(Math.random() * 10) + 1;
+ console.log(randomNumber);
+
+
+// Question 102:
+// Calculate and log the absolute difference between the number -5 and 5.
+
+let absoluteDifference: number = Math.abs(-5 - 5);
+ console.log(absoluteDifference);
